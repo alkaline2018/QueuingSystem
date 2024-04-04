4848088141e164b75ccb1a7625110e8eab5e2780
alkaline2018 <plus159@>
2024-04-04 10:40:20 +0900

    rename: 폴더 이름 변경

--- ch3/clip15/src/main/java/com/example/webflux1/repository/PostCustomR2dbcRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.webflux1.repository;
-
-import reactor.core.publisher.Flux;
-
-public interface PostCustomR2dbcRepository {
-    Flux<Post> findAllByUserId(Long userId);
-}
-
--- ch3/clip15/src/main/java/com/example/webflux1/repository/PostR2dbcRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.webflux1.repository;
-
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import reactor.core.publisher.Flux;
-
-public interface PostR2dbcRepository extends ReactiveCrudRepository<Post, Long>, PostCustomR2dbcRepository{
-    Flux<Post> findByUserId(Long id);
-}
